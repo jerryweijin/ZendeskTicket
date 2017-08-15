ce11d90f14d3f1cdf7f64b5453d6df3a7b9e52d6
Jerry <jerryweijin@hotmail.com>
2017-08-14 19:19:23 -0700

    Added firebase and update the UI after ticket submition.

--- app/src/main/java/com/jerryweijin/zendeskticket/MainActivity.java
+++ app/src/main/java/com/jerryweijin/zendeskticket/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.io.IOException;
 
@@ -25,24 +28,45 @@ public class MainActivity extends AppCompatActivity {
     private Button sendButton;
     private String ticketSubject;
     private String ticketContent;
-
     //justin.chi@ismartalarm.com/token:h5kVKlGBeJ7vXYLp10fJJ5YHS5tu5TtFWc8b5xRM
     //private String credential = "Basic anVzdGluLmNoaUBpc21hcnRhbGFybS5jb20vdG9rZW46aDVrVktsR0JlSjd2WFlMcDEwZkpKNVlIUzV0dTVUdEZXYzhiNXhSTQ==";
-
     //jiafangrong@china.ismartalarm.com/token:h5kVKlGBeJ7vXYLp10fJJ5YHS5tu5TtFWc8b5xRM
     private String credential = "Basic amlhZmFuZ3JvbmdAY2hpbmEuaXNtYXJ0YWxhcm0uY29tL3Rva2VuOmg1a1ZLbEdCZUo3dlhZTHAxMGZKSjVZSFM1dHU1VHRGV2M4YjV4Uk0=";
-
     private String apiUrl = "https://ismartalarm.zendesk.com/api/v2/tickets.json";
     private MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private ProgressBar progressBar;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         ticketSubjectTextView = (TextView) findViewById(R.id.ticketSubjectContent);
         ticketContentTextView = (TextView) findViewById(R.id.ticketDescriptionContent);
         sendButton = (Button) findViewById(R.id.sendButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        ticketSubjectTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "ticketSubjectTextView");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+                Log.i(TAG, "Subject TextView is tapped");
+            }
+        });
+
+        ticketContentTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "ticketContentTextView");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+                Log.i(TAG, "Content TextView is tapped");
+            }
+        });
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,18 +87,42 @@ public class MainActivity extends AppCompatActivity {
                 client.newCall(request).enqueue(new Callback() {
                     @Override
                     public void onFailure(Call call, IOException e) {
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Network unavailable. Please check your network.", Toast.LENGTH_LONG).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        });
                     }
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         if (response.isSuccessful()) {
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ticketSubjectTextView.setText("");
+                                    ticketContentTextView.setText("");
+                                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                            });
                             Log.v(TAG, response.body().string());
                         } else {
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this, "Unable to submit your ticket. Please try agian.", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                }
+                            });
                             throw new IOException("Unexpected code " + response);
-
                         }
+
                     }
                 });
+                progressBar.setVisibility(View.VISIBLE);
             }
         });
     }
